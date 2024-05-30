78fd7782897139692c225a63d807e43105dc408a
Tathagata Nayak <80235249+somu14@users.noreply.github.com>
2024-05-30 22:32:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,45 @@
 const express =require("express");
 const bodyParser=require("body-parser");
-
 const nodemailer=require("nodemailer");
+require("dotenv").config();
 const app=express();
 
 app.use(bodyParser.json());
 
-
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: 'youremail@gmail.com',
-      pass: 'yourpassword'
-    }
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
   });
 
-  app.get("/",(req,res)=>{
-    res.send("your ip:",req.ip);
-  })
-
-app.post("/",(req,res)=>
+  app.get("/",(req,res)=>
 {
-    const mailOptions = {
-        from: 'youremail@gmail.com',
-        to: 'myfriend@yahoo.com',
-        subject: 'Sending Email using Node.js',
-        text: 'That was easy!'
-      };
+    res.send("hi");
 })
 
-app.listen(3000,()=>
+app.post("/send",(req,res)=>
+{
+    const mailOptions = {
+        from: "somu111725@gmail.com",
+        to: req.body.email, 
+        subject: req.body.subject,
+        text: req.body.text,
+      };
+      
+
+      transporter.sendMail(mailOptions, (error, info)=> {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+});
+
+
+app.listen(process.env.PORT,()=>
 {
     console.log("server is running")
 });
