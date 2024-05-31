d66637a8027e9ae1fa6210b585ff6c6034b10796
Tathagata Nayak <80235249+somu14@users.noreply.github.com>
2024-05-31 09:20:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express =require("express");
 const bodyParser=require("body-parser");
 const nodemailer=require("nodemailer");
 require("dotenv").config();
+const cors=require("cors");
 const app=express();
 
 app.use(bodyParser.json());
 
+app.use(cors());
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -23,11 +25,12 @@ app.post("/send",(req,res)=>
 {
     const mailOptions = {
         from: "somu111725@gmail.com",
-        to: req.body.email, 
-        subject: req.body.subject,
-        text: req.body.text,
+        to: req.body.emaildata, 
+        subject: "Thanks for Contacting",
+        text: "Thanks for contacting me "+" "+" subject is: "+req.body.subjectdata+" "+" your text is: "+req.body.bodydata,
       };
       
+      console.log(mailOptions.from,mailOptions.to,mailOptions.subject,mailOptions.text);
 
       transporter.sendMail(mailOptions, (error, info)=> {
         if (error) {
@@ -36,6 +39,8 @@ app.post("/send",(req,res)=>
           console.log('Email sent: ' + info.response);
         }
       });
+
+      res.status(200).send("sent");
 });
 
 
